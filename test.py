d1195e55b29413be6b21611d4eb7f346c584f66c
Sarah Cloughley <smcloughley@gmail.com>
2025-11-04 16:44:08 -0500

    add working examples for arcgis api for python and arcgis maps sdk for javascript

--- test.py
+++ test.py
@@ -1,7 +1,112 @@
-from arcgis.gis import GIS  # type: ignore
+"""
+Find the 5, 10, and 15 minute drive-time polygons around all locations of a grocery store chain in a city.
+"""
 
-gis = GIS()
+import os
+import arcgis
+import pandas as pd
+from dotenv import load_dotenv
 
-public_content = gis.content.search("Fire", item_type="Feature Layer", max_items=5)
+# Load environment variables from .env file
+load_dotenv()
 
-print(public_content)
+
+def print_result(result):
+    """Print useful information from the result."""
+    pd.set_option("display.max_rows", None)
+    pd.set_option("display.max_colwidth", None)
+
+    output_polygons = result.service_areas.sdf
+    print(output_polygons)
+    print("\n-- Output Polygons -- \n")
+    print(output_polygons[["StoreId", "StoreName", "Address",
+                           "FromBreak", "ToBreak"]].to_string(index=False))
+
+
+def main():
+    """Program execution logic."""
+    # inputs
+    facilities = {
+        "displayFieldName": "",
+        "fieldAliases": {
+            "StoreName": "Store Name",
+            "Address": "Address",
+            "StoreId": "Store ID"
+        },
+        "geometryType": "esriGeometryPoint",
+        "spatialReference": {
+            "wkid": 4326,
+            "latestWkid": 4326
+        },
+        "fields": [
+            {
+                "name": "StoreName",
+                "type": "esriFieldTypeString",
+                "alias": "Name",
+                "length": 50
+            },
+            {
+                "name": "Address",
+                "type": "esriFieldTypeString",
+                "alias": "Name",
+                "length": 256
+            },
+            {
+                "name": "StoreId",
+                "type": "esriFieldTypeString",
+                "alias": "Store ID",
+                "length": 16
+            }
+        ],
+        "features": [
+            {
+                "attributes": {
+                    "StoreName": "Store 1",
+                    "Address": "1775 E Lugonia Ave, Redlands, CA 92374",
+                    "StoreId": "120"
+                },
+                "geometry": {
+                    "x": -117.14002999994386,
+                    "y": 34.071219999994128
+                }
+            },
+            {
+                "attributes": {
+                    "StoreName": "Store 2",
+                    "Address": "1536 Barton Rd, Redlands, CA 92373",
+                    "StoreId": "130"
+                },
+                "geometry": {
+                    "x": -117.207329999671,
+                    "y": 34.047980000203609
+                }
+            },
+            {
+                "attributes": {
+                    "StoreName": "Store 3",
+                    "Address": "11 E Colton Ave, Redlands, CA 92374",
+                    "StoreId": "121"
+                },
+                "geometry": {
+                    "x": -117.18194000041973,
+                    "y": 34.06351999976232
+                }
+            }
+        ]
+    }
+
+
+    # Connect to the Service area service
+    api_key = os.getenv("ARCGIS_API_KEY")
+    arcgis.GIS("https://www.arcgis.com", api_key=api_key)
+
+    # Call the Service Area service
+    result = arcgis.network.analysis.generate_service_areas(facilities=facilities,
+                                                            break_values="5 10 15",
+                                                            travel_direction="Towards Facility")
+    print_result(result)
+
+
+
+if __name__ == "__main__":
+    main()
